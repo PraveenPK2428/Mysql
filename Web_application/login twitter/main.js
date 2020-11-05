acb0ef736a28b6e32812e1fd87d111ea68923fa2
Praveen Kumar <praveenkumar.anbalagan98@gmail.com>
2020-11-05 08:19:16 +0530

    web_application

---
+++ Web_application/login twitter/main.js
@@ -0,0 +1,33 @@
+
+function clicked()
+{
+	var name=document.getelementbyid("username");
+	var pswd=document.getelementbyid("password");
+	
+	var name="pk";
+	var pswd="123";
+
+	{
+		if(user.value == name);
+			{
+				if(pswd.value==pswd);
+				{
+					window.alert("login successfully);
+					window.open("http://www.google.com");
+				}
+					else
+						{
+							window.alert("incorrect password");
+						}
+	
+			}	
+							else
+								{
+									window.alert("Incorrect password");
+								}
+			
+	}
+
+}
+
+
